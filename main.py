b417e6eefc4c31c4998c8a9ec3fe36af4ca5ab1a
audi <audi.car@qq.com>
2021-01-23 10:04:43 +0800

    modify logic

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ if __name__ == '__main__':
         start_tool.make_reserve()
     elif choice_function == '2':
         start_tool.login()
+        start_tool.make_reserve()
         start_tool.request_seckill_url()
         start_tool.request_seckill_checkout_page()
         start_tool.submit_seckill_order()
